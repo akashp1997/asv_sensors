ebca8f40952a92e51ab5b9b501b560bac14fa4ef
Akash Purandare <akash.p1997@gmail.com>
2018-03-22 15:57:14 +0530

    Completed IMU Comms for AHRS-8{

--- scripts/imu_comm.py
+++ scripts/imu_comm.py
@@ -7,91 +7,28 @@ import rospy
 
 #Import ROS messages here
 import sensor_msgs.msg
-
-class AutoSerial(object):
-	def __init__(self, device_type=0, baud=9600, ran=[0,100], no_count=10):
-		"""0 for IMU
-		1 for GPS
-		2 for Arduino"""
-		self.device_type = device_type
-		self.baud = baud
-		self.range = ran
-		self.count = no_count
-
-	def get_port(self):
-		self.port_prefix = ["/dev/ttyUSB", "/dev/ttyUSB", "/dev/ttyACM"]
-		for no_iter in range(self.count):
-			for port_no in range(self.range[0], self.range[1]):
-				rate = rospy.Rate(100)
-				#Start IMU Test
-				try:
-					rate.sleep()
-					self.port = serial.Serial(self.port_prefix[self.device_type]+str(port_no), baudrate=self.baud)
-					self.port.flush()				
-					self.port.write("currentTime di.\r\n")
-					line = self.port.read_until(terminator="OK")
-					if line.split("\n")[1].startswith("currentTime ="):
-						self.port.flush()
-						self.port.close()
-						return self.port_prefix[self.device_type]+str(port_no)
-				except serial.serialutil.SerialException:
-					continue
-				#End IMU Test
-		return None
-
 class ImuSensor(object):
-	def __init__(self, baud=115200, topic_name="/imu_data", temp_topic="/temperature", rate=100):
+	def __init__(self, baud=115200, topic_name="/imu_data", temp_topic="/temperature", rate=100, port_name="/dev/ttyUSB0"):
+		"""Constructor to initiate data members"""
 		self.imu_data = sensor_msgs.msg.Imu()
 		self.temp_data = sensor_msgs.msg.Temperature()
-		self.start_port()
-		if self.port==None:
-			rospy.logwarn("IMU Sensor is disconnected.")
+		self.port = serial.Serial("/dev/ttyUSB0", baud)
 		self.pub = rospy.Publisher(topic_name, sensor_msgs.msg.Imu, queue_size=10)
 		self.temp_pub = rospy.Publisher(temp_topic, sensor_msgs.msg.Temperature, queue_size=10)
 		self.rate = rate
 
-	def detect_port(self):
-		self.port = serial.Serial(AutoSerial(baud=115200).get_port(), baudrate=115200)
-		self.port_prefix = ["/dev/ttyUSB", "/dev/ttyUSB", "/dev/ttyACM"]
-		while not rospy.is_shutdown():
-			for port_no in range(self.range[0], self.range[1]):
-				rate = rospy.Rate(100)
-				#Start IMU Test
-				try:
-					rate.sleep()
-					self.port = serial.Serial(self.port_prefix[self.device_type]+str(port_no), baudrate=self.baud)
-					self.port.flush()				
-					self.port.write("currentTime di.\r\n")
-					line = self.port.read_until(terminator="OK")
-					if line.split("\n")[1].startswith("currentTime ="):
-						self.port.flush()
-						self.port.close()
-						return self.port_prefix[self.device_type]+str(port_no)
-				except serial.serialutil.SerialException:
-					continue
-				#End IMU Test
-		return None
-
 	def publish(self):
+		"""Method to publish messages to ROS"""
 		while not rospy.is_shutdown():
-			try:
-				if not self.port.is_open:
-					self.port.open()
-				else:
-					self.get_data()
-					self.pub.publish(self.imu_data)
-					self.temp_pub.publish(self.temp_data)
-					self.port.close()
-			except:
-				self.start_port()
+			self.get_data()
+			self.pub.publish(self.imu_data)
+			self.temp_pub.publish(self.temp_data)
 			rospy.Rate(self.rate).sleep()
 
 	def get_data(self):
-		self.port.flush()
+		"""Read data from the serial port and create a ROS Message"""
 		self.port.write("quaternion di. accelp di. gyrop di. temperature di.\r\n")
 		data = self.port.read_until(terminator="OK").strip().split("\r\n")
-		self.port.flush()
-		#return lines
 		self.imu_data.header.frame_id="base_link"
 		self.imu_data.header.stamp = rospy.Time.now()
 		self.imu_data.orientation.w = float(data[1][18:])
